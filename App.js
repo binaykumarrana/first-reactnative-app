98d23f66eb540ee9862e6a6e711fae3d414072dc
binaykumarrana <binaykumarrana@gmail.com>
2020-07-03 17:32:32 +0800

    Add entered item in list and display

--- App.js
+++ App.js
@@ -1,25 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View , Button} from 'react-native';
+import { StyleSheet, Text, View , Button,TextInput,FlatList} from 'react-native';
 
 export default function App() {
+  const [enteredGoal,setGoal] = useState('');
+  const [courseGoals,setCourseGoal] = useState([]);
 
-  const[outputText,setOutputText] = useState('Open up App.js to start working on your app!');
+  const goalInputHandler=(enteredText) =>{
+    setGoal(enteredText);
+  };
+
+  const var1 = 0;
+  const addGoalHandler = () => {
+    setCourseGoal(currentGoals => [...courseGoals,
+      {key:Math.random(),value:enteredGoal}]);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed!')}/>
-      <StatusBar style="auto"/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} 
+        onChangeText={goalInputHandler}
+        value={enteredGoal}/>
+        <Button title="ADD" onPress={addGoalHandler}/>
+      </View>
+      <FlatList 
+       keyExtractor={(item, index) => String(index)}
+      data={courseGoals} 
+      renderItem={ itemData => (
+        <View style={styles.listItem}>
+          <Text>{itemData.item.value}</Text>
+        </View>
+      )}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen:{
+      padding:48
+    },
+    inputContainer:{
+      flexDirection:'row',
+      justifyContent:'space-between',
+      alignItems:'center'
+    },
+    input:{
+      width:'80%', borderColor:'black', borderWidth:1, padding:10
+    },
+    listItem:{
+      padding:10,
+      margin:10,
+      backgroundColor:'#ccc',
+      borderColor:'black',
+      borderWidth:1
+    }
 });
